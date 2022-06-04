b73617420668fc353623ef887de3033585a047af
maxiemar <denishicmihail@gmail.com>
2022-06-04 14:30:19 +0300

    fix: fix authentication guard

--- src/app/_guards/authentication-guard.service.ts
+++ src/app/_guards/authentication-guard.service.ts
@@ -2,23 +2,24 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { BackEndService } from "../_services/back-end.service";
+import { MatDialog } from "@angular/material/dialog";
+import { SignInDialogComponent } from "../_dialogs/sign-in-dialog/sign-in-dialog.component";
 
 @Injectable({
     providedIn: "root",
 })
 export class AuthenticationGuard implements CanActivate {
-    constructor(private backEndService: BackEndService) {}
+    constructor(private dialog: MatDialog, private backEndService: BackEndService) {}
 
     canActivate(
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
     ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        try {
-            this.backEndService.refreshUser();
+        const userProvided = !!this.backEndService.userSubject.getValue();
+        if (userProvided) {
             return true;
-        } catch (error) {
-            console.log();
         }
-        return this.backEndService.refreshUser();
+        this.dialog.open(SignInDialogComponent);
+        return false;
     }
 }
