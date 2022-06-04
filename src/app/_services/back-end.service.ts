850ee3cfc58f47e3de45a46be4c7b7f80b39158a
maxiemar <denishicmihail@gmail.com>
2022-06-04 23:09:49 +0300

    fix: share observables to avoid multiple execution on subscriptions

--- src/app/_services/back-end.service.ts
+++ src/app/_services/back-end.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { BehaviorSubject, lastValueFrom, mergeMap, Observable, Subject } from "rxjs";
-import { map } from "rxjs/operators";
+import { BehaviorSubject, lastValueFrom, Observable, Subject } from "rxjs";
+import { map, mergeMap, share } from "rxjs/operators";
 
 import { User } from "../_models/user";
 import { Group } from "../_models/group";
@@ -24,13 +24,13 @@ export class BackEndService {
         this.refreshUser();
     }
 
-    fetchUserGroups(): Observable<Group[]> {
+    fetchUserGroups() {
         console.log("Fetching groups...");
         const user = this.userSubject.getValue();
         if (!user) {
             throw new Error("User is undefined!");
         }
-        return this.http.get<Group[]>(`${environment.apiUrl}/users/${user.id}/groups`, { withCredentials: true });
+        return this.http.get<Group[]>(`${environment.apiUrl}/users/${user.id}/groups`, { withCredentials: true }).pipe(share());
     }
 
     fetchGroupQueuesPerspective() {
@@ -43,9 +43,11 @@ export class BackEndService {
         if (!user) {
             throw new Error("User is undefined!");
         }
-        return this.http.get<PerspectiveQueue[]>(`${environment.apiUrl}/groups/${group.id}/queues/perspective/${user.id}`, {
-            withCredentials: true,
-        });
+        return this.http
+            .get<PerspectiveQueue[]>(`${environment.apiUrl}/groups/${group.id}/queues/perspective/${user.id}`, {
+                withCredentials: true,
+            })
+            .pipe(share());
     }
 
     fetchGroupUsers(): Observable<User[]> {
@@ -54,7 +56,7 @@ export class BackEndService {
         if (!group) {
             throw new Error("Group is undefined!");
         }
-        return this.http.get<User[]>(`${environment.apiUrl}/groups/${group.id}/members`, { withCredentials: true });
+        return this.http.get<User[]>(`${environment.apiUrl}/groups/${group.id}/members`, { withCredentials: true }).pipe(share());
     }
 
     fetchQueueUsers(queueId: string | undefined): Observable<User[]> {
@@ -65,20 +67,16 @@ export class BackEndService {
             throw new Error("Queue in undefined!");
         }
 
-        return this.http.get<User[]>(`${environment.apiUrl}/queues/${neededId}/members`, {
-            withCredentials: true,
-        });
+        return this.http
+            .get<User[]>(`${environment.apiUrl}/queues/${neededId}/members`, {
+                withCredentials: true,
+            })
+            .pipe(share());
     }
 
-    async refreshUser(): Promise<boolean> {
-        try {
-            const user = await lastValueFrom(this.fetchUser());
-            this.userSubject.next(user);
-            return true;
-        } catch (error) {
-            console.error(error);
-            return false;
-        }
+    async refreshUser(): Promise<void> {
+        const user = await lastValueFrom(this.fetchUser());
+        this.userSubject.next(user);
     }
 
     createQueue(queueInfo: QueueInfo, groupId?: string) {
@@ -87,14 +85,18 @@ export class BackEndService {
         if (!neededGroupId) {
             throw new Error("Group is undefined!");
         }
-        return this.requestQueueCreate(neededGroupId, queueInfo).subscribe(() => this.refreshGroup());
+        const observableQueue = this.requestQueueCreate(neededGroupId, queueInfo);
+        observableQueue.subscribe(() => this.refreshGroup());
+        return observableQueue;
     }
 
     createGroup(groupInfo: GroupInfo) {
-        return this.requestGroupCreate(groupInfo).pipe(mergeMap(() => this.fetchUserGroups()));
+        return this.requestGroupCreate(groupInfo)
+            .pipe(mergeMap(() => this.fetchUserGroups()))
+            .pipe(share());
     }
 
-    refreshGroup(groupId?: string): void {
+    refreshGroup(groupId?: string) {
         const group = this.groupSubject.getValue();
         const neededGroupId = groupId ? groupId : group?.id;
         if (!neededGroupId) {
@@ -103,7 +105,7 @@ export class BackEndService {
         this.fetchGroup(neededGroupId).subscribe((waitedGroup) => this.groupSubject.next(waitedGroup));
     }
 
-    refreshGroupByQueue(queueId: string): void {
+    refreshGroupByQueue(queueId: string) {
         if (this.groupSubject.getValue()) {
             this.refreshGroup();
         } else {
@@ -111,7 +113,7 @@ export class BackEndService {
         }
     }
 
-    refreshQueue(queueId?: string): void {
+    refreshQueue(queueId?: string) {
         const queue = this.queueSubject.getValue();
         const neededQueueId = queueId ? queueId : queue?.id;
         if (!neededQueueId) {
@@ -120,23 +122,26 @@ export class BackEndService {
         this.fetchQueue(neededQueueId).subscribe((waitedQueue) => this.queueSubject.next(waitedQueue));
     }
 
-    shuffleQueue(): void {
+    shuffleQueue() {
         const queue = this.queueSubject.getValue();
         if (!queue) {
             throw new Error("Queue in undefined!");
         }
         const queueObservable = this.requestQueueShuffle(queue.id);
         queueObservable.subscribe((waitedQueue) => this.queueSubject.next(waitedQueue));
+        return queueObservable;
     }
 
     fetchGroupByQueue(queueId: string): Observable<Group> {
         console.log("Fetching group from queue...");
-        return this.http.get<Group>(`${environment.apiUrl}/queues/${queueId}/group`, {
-            withCredentials: true,
-        });
+        return this.http
+            .get<Group>(`${environment.apiUrl}/queues/${queueId}/group`, {
+                withCredentials: true,
+            })
+            .pipe(share());
     }
 
-    rotateQueue(): void {
+    rotateQueue() {
         const queue = this.queueSubject.getValue();
         if (!queue) {
             throw new Error("Queue is undefined!");
@@ -151,6 +156,7 @@ export class BackEndService {
             const nextMember = waitedQueue.members[waitedQueue.members.length - 1];
             this.queueMemberInActionSubject.next(nextMember);
         });
+        return queueObservable;
     }
 
     showQueueMember() {
@@ -173,6 +179,7 @@ export class BackEndService {
         const queueId = queue.id;
         const queueMemberObservable = this.requestQueueAddMember(queueId, user.id);
         queueMemberObservable.subscribe(() => this.fetchQueue(queue.id).subscribe((waitedQueue) => this.queueSubject.next(waitedQueue)));
+        return queueMemberObservable;
     }
 
     addUserToGroup(userEmail: string) {
@@ -180,7 +187,9 @@ export class BackEndService {
         if (!group) {
             throw new Error("Group is undefined!");
         }
-        this.requestGroupAddUser(group.id, userEmail).subscribe(() => this.refreshGroup());
+        const observable = this.requestGroupAddUser(group.id, userEmail);
+        observable.subscribe(() => this.refreshGroup());
+        return observable;
     }
 
     removeMemberFromQueue(userId?: string) {
@@ -274,86 +283,90 @@ export class BackEndService {
 
     private fetchUser() {
         console.log("Fetching user...");
-        return this.http.get<User>(`${environment.apiUrl}/users/token`, { withCredentials: true });
+        return this.http.get<User>(`${environment.apiUrl}/users/token`, { withCredentials: true }).pipe(share());
     }
 
     private fetchGroup(groupId: string) {
         console.log("Fetching group...");
-        return this.http.get<Group>(`${environment.apiUrl}/groups/${groupId}`, { withCredentials: true });
+        return this.http.get<Group>(`${environment.apiUrl}/groups/${groupId}`, { withCredentials: true }).pipe(share());
     }
 
     private fetchQueue(queueId: string) {
         console.log("Fetching queue...");
-        return this.http.get<Queue>(`${environment.apiUrl}/queues/${queueId}`, { withCredentials: true });
+        return this.http.get<Queue>(`${environment.apiUrl}/queues/${queueId}`, { withCredentials: true }).pipe(share());
     }
 
     private fetchQueueMember(queueId: string, userId: string) {
         console.log("Fetching queue member...");
-        return this.http.get<LightWeightQueueMember>(`${environment.apiUrl}/queues/${queueId}/members/${userId}`, {
-            withCredentials: true,
-        });
+        return this.http
+            .get<LightWeightQueueMember>(`${environment.apiUrl}/queues/${queueId}/members/${userId}`, {
+                withCredentials: true,
+            })
+            .pipe(share());
     }
 
     private requestQueueCreate(groupId: string, queueInfo: QueueInfo) {
         console.log("Creating queue...");
-        return this.http.post<Queue>(`${environment.apiUrl}/groups/${groupId}/queues`, queueInfo, { withCredentials: true });
+        return this.http.post<Queue>(`${environment.apiUrl}/groups/${groupId}/queues`, queueInfo, { withCredentials: true }).pipe(share());
     }
 
     private requestQueueShuffle(queueId: string) {
         console.log("Shuffling queue...");
-        return this.http.post<Queue>(`${environment.apiUrl}/queues/${queueId}/shuffle`, null, { withCredentials: true });
+        return this.http.post<Queue>(`${environment.apiUrl}/queues/${queueId}/shuffle`, null, { withCredentials: true }).pipe(share());
     }
 
     private requestQueueRotate(queueId: string) {
         console.log("Rotating queue...");
-        return this.http.post<Queue>(`${environment.apiUrl}/queues/${queueId}/rotate`, null, { withCredentials: true });
+        return this.http.post<Queue>(`${environment.apiUrl}/queues/${queueId}/rotate`, null, { withCredentials: true }).pipe(share());
     }
 
     private requestQueueAddMember(queueId: string, userId: string) {
         console.log("Adding queue member...");
-        return this.http.put<QueueMember>(`${environment.apiUrl}/queues/${queueId}/members/${userId}`, null, { withCredentials: true });
+        return this.http
+            .put<QueueMember>(`${environment.apiUrl}/queues/${queueId}/members/${userId}`, null, { withCredentials: true })
+            .pipe(share());
     }
 
     private requestQueueRemoveMember(queueId: string, userId: string) {
         console.log("Removing queue member...");
-        return this.http.delete<void>(`${environment.apiUrl}/queues/${queueId}/members/${userId}`, { withCredentials: true });
+        return this.http.delete<void>(`${environment.apiUrl}/queues/${queueId}/members/${userId}`, { withCredentials: true }).pipe(share());
     }
 
     private requestQueueMemberStatusUpdate(queueId: string, userId: string, status: boolean) {
         console.log("Setting member status...");
-        return this.http.patch<{ id: string; status: boolean }>(
-            `${environment.apiUrl}/queues/${queueId}/members/${userId}`,
-            { status: status },
-            { withCredentials: true }
-        );
+        return this.http
+            .patch<{ id: string; status: boolean }>(
+                `${environment.apiUrl}/queues/${queueId}/members/${userId}`,
+                { status: status },
+                { withCredentials: true }
+            )
+            .pipe(share());
     }
 
     private requestQueueDelete(groupId: string, queueId: string) {
         console.log("Deleting queue...");
-        return this.http.delete<void>(`${environment.apiUrl}/groups/${groupId}/queues/${queueId}`, { withCredentials: true });
+        return this.http.delete<void>(`${environment.apiUrl}/groups/${groupId}/queues/${queueId}`, { withCredentials: true }).pipe(share());
     }
 
     private requestGroupCreate(groupInfo: GroupInfo) {
         console.log("Creating group...");
-        return this.http.post<Group>(`${environment.apiUrl}/groups`, groupInfo, { withCredentials: true });
+        return this.http.post<Group>(`${environment.apiUrl}/groups`, groupInfo, { withCredentials: true }).pipe(share());
     }
 
     private requestGroupAddUser(groupId: string, userEmail: string) {
         console.log("Adding user to group...");
-        return this.http.put<void>(
-            `${environment.apiUrl}/groups/${groupId}/members/email`,
-            { email: userEmail },
-            { withCredentials: true }
-        );
+        return this.http
+            .put<void>(`${environment.apiUrl}/groups/${groupId}/members/email`, { email: userEmail }, { withCredentials: true })
+            .pipe(share());
     }
 
     private requestGroupRemoveMember(groupId: string, userId: string) {
         console.log("Removing member from group...");
-        return this.http.delete<void>(`${environment.apiUrl}/groups/${groupId}/members/${userId}`, { withCredentials: true });
+        return this.http.delete<void>(`${environment.apiUrl}/groups/${groupId}/members/${userId}`, { withCredentials: true }).pipe(share());
     }
 
     private requestGroupDelete(groupId: string) {
         console.log("Deleting group...");
-        return this.http.delete<void>(`${environment.apiUrl}/groups/${groupId}`, { withCredentials: true });
+        return this.http.delete<void>(`${environment.apiUrl}/groups/${groupId}`, { withCredentials: true }).pipe(share());
     }
 }
